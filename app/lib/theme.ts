3a0ab544b2ce32bb29a53c90d12044a4983324dd
Victor Moura <contatomeucardapio@email.com>
2026-06-17 18:02:52 -0400

    melhorias cardapio e pagina carrinho

--- app/lib/theme.ts
+++ app/lib/theme.ts
@@ -1,25 +1,6 @@
 export function getThemeSettings() {
-
-  if (typeof window === "undefined") {
-
-    return {
-  lightMode: true,
-  selectedColor: "#6D1F2F",
-}
-  }
-
-  const lightMode =
-    localStorage.getItem(
-      "cardapio-light-mode"
-    ) === "true"
-
-  const selectedColor =
-    localStorage.getItem(
-      "cardapio-primary-color"
-    ) || "#7F1D1D"
-
   return {
-    lightMode,
-    selectedColor,
+    lightMode: true,
+    selectedColor: "#6D1F2F",
   }
 }
