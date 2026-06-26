04c337d0bfb6b46e3e9cf55235b3f3ab6a605613
Victor Moura <contatomeucardapio@email.com>
2026-06-26 11:09:53 -0400

    Atualiza pedido via webhook Pagar.me

--- app/api/pagarme/webhook/route.ts
+++ app/api/pagarme/webhook/route.ts
@@ -5,9 +5,48 @@ export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
 
+    const orderId =
+  body?.data?.order?.id;
+
+const paymentStatus =
+  body?.data?.status;
+
+console.log("ORDER ID:", orderId);
+console.log("STATUS:", paymentStatus);
+
     console.log("========== WEBHOOK PAGAR.ME ==========");
     console.log(JSON.stringify(body, null, 2));
     console.log("======================================");
+
+    if (
+  orderId &&
+  paymentStatus === "paid"
+) {
+  const { error } =
+    await supabaseAdmin
+      .from("pedidos")
+      .update({
+        payment_status:
+          "approved",
+
+        status: "aceito",
+      })
+      .eq(
+        "pagarme_order_id",
+        orderId
+      );
+
+  if (error) {
+    console.error(
+      "Erro ao atualizar pedido:",
+      error
+    );
+  } else {
+    console.log(
+      "Pedido atualizado com sucesso!"
+    );
+  }
+}
 
     return NextResponse.json(
       {
